b10b59e65da76fcf597157e86e32341dc8073bb8
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-04-15 19:35:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", "")) #API ID from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH", "") #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # Bot token from @BotFather
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else [] #Keep thia empty otherwise bot will not work for owner.
+API_ID = int(os.environ.get("API_ID", "26180065")) #API ID from https://my.telegram.org/auth
+API_HASH = os.environ.get("API_HASH", "0a6358307acf8d0d2bf98b6827e0f5c7") #API Hash from https://my.telegram.org/auth
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6992264465:AAFLUE_QV50Ly9OBpp3IGAo_mkMyfl984-4") # Bot token from @BotFather
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "6072149828").split(",")] if os.environ.get("ADMINS") else [] #Keep thia empty otherwise bot will not work for owner.
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Greylinks")
-DATABASE_URL = os.environ.get("DATABASE_URL", "") # mongodb uri from https://www.mongodb.com/
-OWNER_ID =  int(os.environ.get("OWNER_ID", "")) # id of the owner
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "shortener")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://errorsmonster:S.Aruna1155182089@shortener.woficdt.mongodb.net/?retryWrites=true&w=majority") # mongodb uri from https://www.mongodb.com/
+OWNER_ID =  int(os.environ.get("OWNER_ID", "6072149828")) # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(6390495622)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # log channel for information about users
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "GreyMattersTech") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002115739348")) # log channel for information about users
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MrAK_LinkZz") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "False") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://telegra.ph/file/19eeb26fa2ce58765917a.jpg') # image when someone hit /start
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://graph.org/file/8cd764fbdf3ccd34abe22.jpg') # image when someone hit /start
 LINK_BYPASS = "True" 
 """
    _____                    __  __         _    _              _       _______           _     
